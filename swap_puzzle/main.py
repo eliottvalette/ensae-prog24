bfffb31e71fe23bd8242585fe4f81d3a72eac6b9
Valette Eliott <eliott.valette@hec.edu>
2024-02-12 08:41:23 +0100

    main

--- swap_puzzle/main.py
+++ swap_puzzle/main.py
@@ -1,12 +1,20 @@
 from grid import Grid
+from graph import Graph
 
 g = Grid(2, 3)
 print(g)
 
-data_path = "../input/"
+data_path = "input/"
 file_name = data_path + "grid0.in"
 
 print(file_name)
 
 g = Grid.grid_from_file(file_name)
 print(g)
+
+
+graph_file_name = data_path + "graph1.in"
+graph = Graph.graph_from_file(graph_file_name)
+
+print(graph.nodes)
+print(graph)
